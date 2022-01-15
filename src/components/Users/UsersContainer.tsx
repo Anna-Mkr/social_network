787e7658d907d434dabab0b97efb8c109b4d5a4c
Anna-Mkr <annamk91@mail.ru>
2022-01-15 15:08:24 +0300

    added page pagination

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,27 +1,35 @@
-
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state: any) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
 let mapDispatchToProps = (dispatch: any) => {
     return {
-follow: (userId: any) => {
-    dispatch(followAC(userId));
-},
+        follow: (userId: any) => {
+            dispatch(followAC(userId));
+        },
         unfollow: (userId: any) => {
             dispatch(unfollowAC(userId))
         },
-        setUsers: (users: any) => {
+        setUsers: (users: []) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber: number) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount: number) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         },
     }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps) (Users);
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
