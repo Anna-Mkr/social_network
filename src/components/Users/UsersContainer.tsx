742ea4073a7a040da3fc56efa8a98fc0ec6d7c9b
Anna-Mkr <annamk91@mail.ru>
2022-01-16 11:18:16 +0300

    merged two container components UsersAPIComponet and UsersContainer

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,42 @@
 
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
-import UsersAPIComponent from "./UsersAPIComponent";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersAPIComponent extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+
+
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+
+            this.props.setUsers(response.data.items);
+
+        });
+    }
+
+    render() {
+
+
+        return <Users onPageChanged={this.onPageChanged} currentPage={this.props.currentPage} users={this.props.users}
+                      pageSize={this.props.pageSize} follow={this.props.follow} totalUsersCount={this.props.totalUsersCount}
+                      unfollow={this.props.unfollow}/>
+
+    }
+}
 
 let mapStateToProps = (state: any) => {
     return {
